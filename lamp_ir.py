71a4ce4e42b69d699b2c2fa72912cc14ce5d215b
Flávio Caetano <flavio@vieiracaetano.com>
2018-02-09 19:13:54 -0200

    Python calling local lircrc

--- lamp_ir.py
+++ lamp_ir.py
@@ -16,14 +16,15 @@ gpio.setwarnings(False)
 gpio.setmode(gpio.BCM)
 [ gpio.setup(pin, gpio.OUT) for pin in PINS.values() ]
 
-sockid = lirc.init("lamps")
+lirc.init("lamps")
+lirc.load_config_file("lircrc")
 
 while True:
 	pin = lirc.nextcode()[0]
-	
+
 	newValue = not gpio.input(PINS[pin])
 	gpio.output(PINS[pin], newValue)
 
 	print "Turning " + pin + " lamp " + ("on" if newValue else "off")
 
-lirc.deinit() 
+lirc.deinit()
