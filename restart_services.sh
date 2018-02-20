613ac8c3f6be44cd131cbdc304153ef9091e33b7
Flávio Caetano <flavio@vieiracaetano.com>
2018-02-20 18:55:59 -0300

    Fix: lamp_ir not restarting

--- restart_services.sh
+++ restart_services.sh
@@ -1,11 +1,7 @@
 #! /bin/sh
 
-systemctl stop homebridge
-systemctl stop lirc
-systemctl stop lamp_ir
-
 rm /root/.homebridge/accessories/cachedAccessories
 
-systemctl start homebridge
-systemctl start lirc
-systemctl start lamp_ir
+systemctl restart homebridge
+systemctl restart lirc
+systemctl restart lamp_ir
