c7667b40bb145596a9f0fba6ce3b8440c308bcdb
Flávio Caetano <flavio@vieiracaetano.com>
2018-03-01 19:47:13 -0300

    Fix: Avoid unnecessary installs
    fix #1

--- restart_services.sh
+++ restart_services.sh
@@ -4,30 +4,35 @@ updated_recently() {
   NOW=$(date +'%Y-%m-%d %H:%M')
   FILE_DATE=$(date -r $1 +'%Y-%m-%d %H:%M')
 
-  echo "$1 - $FILE_DATE"
-
   [ "$NOW" = "$FILE_DATE" ]
 }
 
-echo "Restart services - $(date +'%Y-%m-%d %H:%M')"
+if updated_recently 'requirements.txt'; then
+  source venv/bin/activate
+  pip install -r requirements.txt
+fi
 
 if updated_recently 'homebridge.config'; then
   echo "Restarting Homebridge"
   rm /root/.homebridge/accessories/cachedAccessories
-  systemctl restart homebridge
+  sudo systemctl stop homebridge
+  sudo systemctl start homebridge
 fi
 
 if updated_recently 'lircrc'; then
   echo "Restarting lirc"
-  systemctl restart lirc
+  sudo systemctl stop lirc
+  sudo systemctl start lirc
 fi
 
 if updated_recently 'lamp_ir.py'; then
   echo "Restarting lamp_ir"
-  systemctl restart lamp_ir
+  sudo systemctl stop lamp_ir
+  sudo systemctl start lamp_ir
 fi
 
 if updated_recently 'dingdong.py'; then
   echo "Restarting dingdong"
-  systemctl restart dingdong
+  sudo systemctl stop dingdong
+  sudo systemctl start dingdong
 fi
